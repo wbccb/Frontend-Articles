c07c22e98c090896e890e96ea08b1bf7dd4b2e6d
wbc <wbccb18814122@163.com>
2023-03-14 00:18:13 +0800

    webpack-debugger: 增加内联的loader解析

--- webpack-debugger/js-enhanced_resolve/src/entry1.js
+++ webpack-debugger/js-enhanced_resolve/src/entry1.js
@@ -1,5 +1,5 @@
 import {getG} from "aliasTest/common_____g";
 import _ from "loadsh";
-import {dirIndex} from "./dir";
+import {dirIndex} from "babel-loader!./dir?query=1#location";
 
 console.info(getG() + _.add(3, 4) + dirIndex);
