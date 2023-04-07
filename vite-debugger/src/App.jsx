9af73d7ae4c9b62c0424bbfb4a8cc2dbd7cff893
wbc <wbccb18814122@163.com>
2023-04-07 22:59:05 +0800

    vite-debugger: 增加调试的.vue文件

--- vite-debugger/src/App.jsx
+++ vite-debugger/src/App.jsx
@@ -1,10 +1,14 @@
 import { defineComponent } from "vue";
-
+import Index from "./Index.vue";
 export default defineComponent({
+    components: [Index],
     setup() {
         return () => {
             return (
-                <div>这是App.jsx</div>
+               <div>
+                   <div>这是App.jsx</div>
+                   <Index></Index>
+               </div>
             )
         };
     },
---
+++ vite-debugger/src/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index-wrapper">这是Index.vue</div>
+</template>
+<script>
+export default {
+  name: "Index",
+  setup() {
+    const indexData = "index Data";
+    return {}
+  }
+}
+</script>
+<style scoped>
+.index-wrapper {
+  background-color: rebeccapurple;
+}
+</style>
