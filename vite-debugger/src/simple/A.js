2fcbef68f2b4d95bfedf9ed842bb7d64d31d22d3
wbc <wbccb18814122@163.com>
2023-04-16 22:25:41 +0800

    vite-debugger: 增加调试代码

--- vite-debugger/src/simple/A.js
+++ vite-debugger/src/simple/A.js
@@ -1,3 +1,3 @@
-import {test} from "./B.js";
-console.info("A.js", test);
-export const AExport = "AExport";
+import {test1} from "./B.js";
+console.info("A.js", test1);
+export const AExport = "AExport3";
--- vite-debugger/src/simple/B.js
+++ vite-debugger/src/simple/B.js
@@ -1,7 +1,7 @@
-const test = "B.js3";
+const test = "B.js";
 import.meta.hot.acceptExports("test", (mod)=>{
     console.error("B.js热更新触发");
 })
 // 当acceptExports覆盖了所有export数据时，会强行设置isSelfAccepting=true
-const test1 = "B2.js";
+const test1 = "B5.js";
 export {test, test1}
