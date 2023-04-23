3a212d8f31da708c98d63c5d268e589c5b15dba8
wbc <wbccb18814122@163.com>
2023-04-23 14:06:01 +0800

    手写: 完成miniReact的Render and Commit Phases的手写

--- work-and-study-notes/react/miniReact.js
+++ work-and-study-notes/react/miniReact.js
@@ -41,7 +41,7 @@ const miniReact = {
             }
         }
     },
-    render(element, container) {
+    createDom(element, container) {
         const dom = element.type === ELEMENT_TYPE.TEXT_ELEMENT
             ? document.createTextNode("")
             : document.createElement(element.type);
@@ -53,11 +53,37 @@ const miniReact = {
                dom[name] = element.props[name];
             });
 
-        element.props.children.forEach(child=> {
-            this.render(child, dom);
-        });
+        // element.props.children.forEach(child=> {
+        //     this.render(child, dom);
+        // });
+        //
+        // container.appendChild(dom);
+        return dom;
+    },
+    wipRoot: null,
+    render(element, container) {
+        // 本来render()是进行DOM的创建！现在改为nextUnitOfWork的赋值
+        // DOM的详细创建方法调用放在performUnitOfWork()中
+        // DOM的详细创建方法放在createDOM()中
+        this.wipRoot = {
+            dom: container,
+            props: {
+                children: [element]
+            }
+        }
+        this.nextUnitOfWork = this.wipRoot;
+    },
+    commitRoot() {
+        this.commitWork(this.wipRoot.child);
+        this.wipRoot = null;
+    },
+    commitWork(fiber) {
+        if(!fiber) return;
 
-        container.appendChild(dom);
+        const domParent = fiber.parent.dom;
+        domParent.appendChild(fiber.dom);
+        this.commitWork(fiber.child);
+        this.commitRoot(fiber.sibling);
     },
     nextUnitOfWork: null,
     workLoop(deadline) {
@@ -72,16 +98,67 @@ const miniReact = {
             shouldYield = deadline.timeRemaining() < 1;
         }
 
+        if(!this.nextUnitOfWork && this.wipRoot) {
+            commitRoot();
+        }
+
         // 如果剩余时间不够了，则调用requestIdleCallback等待浏览器有空闲时间再执行
         requestIdleCallback(this.workLoop);
     },
-    performUnitOfWork(nextUnitOfWork) {
-        // 执行每一个unit的任务
+    performUnitOfWork(fiber) {
+        // 执行每一个unit的任务：
+        if(!fiber.dom) {
+            fiber.dom = this.createDom(fiber);
+        }
+
+        // if(fiber.parent) {
+        //     fiber.parent.dom.appendChild(fiber.dom);
+        // }
+
+        // 新的fiber先寻找它的children数据
+        const elements = fiber.props.children;
+        let index = 0;
+        let prevSibling = null;
+
+        while(index < elements.length) {
+            const element = elements[i];
+
+            const newFiber = {
+                type: element.type,
+                props: element.props,
+                parent: fiber,
+                dom: null
+            }
+
+            if(index === 0) {
+                fiber.child = newFiber;
+            } else {
+                // 此时prevSibling是newFiber的左边元素
+                prevSibling.sibling = newFiber;
+            }
+
+            prevSibling = newFiber;
+            index++;
+        }
+
+        if(fiber.child) {
+            return fiber.child;
+        }
+
+        let nextFiber = fiber;
+        while(nextFiber) {
+            // 新的fiber先寻找它的sibling数据
+            if(nextFiber.sibling) {
+                return nextFiber.sibling;
+            }
+            // 没有children，没有sibling，则直接找它的parent
+            nextFiber = nextFiber.parent;
+        }
     }
+
 };
 
-// 调用requestIdleCallback等待浏览器有空闲时间再执行
-requestIdleCallback(miniReact.workLoop);
+
 
 const element = miniReact.createElement(
     "div",
